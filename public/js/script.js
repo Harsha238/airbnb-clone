dd0c518c68d009d091f8101a2a5cc805c248e969
Harsha238 <harshithasai556@gmail.com>
2026-03-30 15:50:08 +0530

    Improved responsive UI: fixed navbar search, optimized desktop layout, mobile fixes

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,20 @@
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation')
+  const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.style.display =
+    mobileMenu.style.display === "block" ? "none" : "block";
+});
+
+// close when clicking outside
+document.addEventListener("click", (e) => {
+  if (!menuBtn.contains(e.target) && !mobileMenu.contains(e.target)) {
+    mobileMenu.style.display = "none";
+  }
+});
 
   // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
