9d444f24c0bc867a17c2a0dd0780c5154e0807da
Harsha238 <harshithasai556@gmail.com>
2026-03-30 20:17:24 +0530

    Fixed MongoDB connection and DB initialization

--- init/index.js
+++ init/index.js
@@ -1,37 +1,34 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = process.env.MONGO_URI;
+require("dotenv").config();
+const MONGO_URI = process.env.MONGO_URI;
+
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URI);
 }
 
 
 main()
-  .then(() => {
+  .then(async() => {
     console.log("connected to DB");
+    await Listing.deleteMany({});
+    console.log("deleted old data");
+    initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "66b1c0f8d9c7e3b2a1234567"
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+    process.exit();
   })
   .catch((err) => {
     console.log(err);
   });
 
 
-const initDB = async () => {
-  await Listing.deleteMany({});
-
-  initData.data = initData.data.map((obj) => ({
-    ...obj,
-    owner: "66b1c0f8d9c7e3b2a1234567"
-  }));
-
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
-};
-
-initDB();
 
 
 
